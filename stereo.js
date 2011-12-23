106e7e62c903748a8d2b7ea7532aff3d8ed797a1
Nat Guy <nattybumppo@gmail.com>
2011-12-23 02:26:53 -0800

    Added center bar and support for negative values.

--- stereo.js
+++ stereo.js
@@ -6,66 +6,30 @@ var testSign = 1;
 // Config values
 var boxWidth = 44;
 var boxHeight = 8;
+var numBars = 10;
+var maxBoxes = 20;
 var spaceBetweenBoxes = 2;
 var spaceBetweenBars = 4;  
-var timeStep = 64;
-var backgroundColor = "#000000"; 
+var timeStep = 16;
+var backgroundColor = "#000000";
+var centerColor = "#444444";
+var centerHeight = 30;
+var centerHorizontalMargin = 20;
+
 
 function initDraw()
 {
     var canvas = document.getElementById("stereo-canvas");
     var context = canvas.getContext("2d");
 
-    var i;
-    for(i = 0; i < 20; i++)
-    {
-        // alert(i + " of " + 20 + ": " + getColor(i, 20));
-    }
-
     return setInterval(process, timeStep);
 }
 
 function process()
 {
-    /*
-    // Create an imagedata object in order to do
-    // individual pixel manipulation on the canvas
-    var imageData = context.createImageData(canvas.width, canvas.height);
-    var x = 0, y = 0;
-
-    for(y = 0; y < canvas.height; y++)
-    {
-        for(x = 0; x < canvas.width; x++)
-        {
-            // Set pixel colors
-            imageData.data[(y * canvas.width + x) * 4] = red;
-            imageData.data[(y * canvas.width + x) * 4 + 1] = green;
-            imageData.data[(y * canvas.width + x) * 4 + 2] = blue;
-            imageData.data[(y * canvas.width + x) * 4 + 3] = alpha;
-
-            // Change pixel colors
-            red += 1;
-            green += 3;
-            blue--;
-            if (red > 255){ red = 0; }
-            if (green > 255){ green = 0; }
-            if (blue < 0){ blue = 255; }
-        }
-    }
-    context.putImageData(imageData, 0, 0);
-
-    */
-
-    var numBars = 10;
 
     var barNo = 0;
     var maxLevel = 1000;
-
-
-    level += 5 * testSign;
-    if (level > maxLevel){ testSign *= -1; }
-
-    if (level < 0){ testSign *= -1; }
 
     draw(numBars, barNo, maxLevel);
 
@@ -77,33 +41,81 @@ function draw(numBars, barNo, maxLevel)
     var context = canvas.getContext("2d")
 
     context.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Draw bars
     for(barNo = 0; barNo < numBars; barNo++)
     {
-        level += 10 * testSign;
-        if (level > maxLevel){ testSign *= -1; }
+        // level += 10 * testSign;
+        // if (level > maxLevel){ testSign *= -1; }
 
-        if (level < 0){ testSign *= -1; }
+        // if (level < 0){ testSign *= -1; }
 
-        drawBar(barNo, level, true, maxLevel, 20);
+
+        level += 3 * testSign;
+        // If the level is beyond the maximum level, and positive,
+        // make it start going start
+        if (level > maxLevel)
+        {
+            testSign = -1;
+        }
+
+        // If the level is beyond the maximum level, and negative,
+        // make it start going up
+        if (level < maxLevel * (-1))
+        {
+            testSign = 1;
+        }
+
+        drawBar(barNo, level, maxLevel, maxBoxes);
+        // testSign *= -1;
     }
+
+    // Draw center
+    var centerPos = canvas.height / 2;
+    var centerWidth = boxWidth * numBars + spaceBetweenBars * (numBars - 1) + centerHorizontalMargin * 2;
+
+    context.fillStyle = centerColor;
+    context.fillRect(0, centerPos - centerHeight / 2, centerWidth, centerHeight);
 
 }
 
 // Maps level to a box level, and draws it
-function drawBar(barNo, level, sign, maxLevel, maxBoxes)
+function drawBar(barNo, level, maxLevel, maxBoxes)
 {
+    // alert("barNo, level, sign, maxLevel, maxBoxes: " + barNo + " " + level + " " + sign + " " + maxLevel + " " + maxBoxes);
+
     var canvas = document.getElementById("stereo-canvas");
     var context = canvas.getContext("2d")
 
     boxesToLight = maxBoxes * (level / maxLevel);
+    // In case boxesToLight was multiplied by a negative level, reset to positive
+    if (boxesToLight < 0)
+    {
+        boxesToLight *= -1;
+    }
+
     // alert (boxesToLight);
 
     var boxNo;
 
     for(boxNo = 0; boxNo < boxesToLight; boxNo++)
     {
-        x = 45 + (boxWidth + spaceBetweenBars) * barNo;
-        y = canvas.height - (45 + (boxHeight + spaceBetweenBoxes) * boxNo);
+        x = centerHorizontalMargin + (boxWidth + spaceBetweenBars) * barNo;
+
+
+        var centerPos = canvas.height / 2;
+        if(level > 0)
+        {
+            // alert("positive");
+            // Draw from the center up
+            y = centerPos - (boxHeight + spaceBetweenBoxes) - (centerHeight / 2 + (boxHeight + spaceBetweenBoxes) * boxNo);
+        }
+        else
+        {
+            // alert("negative");
+            // Draw from the center down
+            y = centerPos + spaceBetweenBoxes + (centerHeight / 2 + (boxHeight + spaceBetweenBoxes) * boxNo);
+        }
         // alert("Drawing at x: " + x + " and y: " + y + " and boxNo: " + boxNo);
         context.fillStyle = getColor(boxNo, maxBoxes);
         context.fillRect(x, y, boxWidth, boxHeight);
